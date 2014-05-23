61bc8215d3e788d1bd3ff97cc749dc35efd4c4c6
Noitidart <noitidart@gmail.com>
2014-05-22 21:41:52 -0700

    1.0.rev12
    -got in window listener
    -moved css register to loadSheet method
    -it is also taking over the onMouseOver function now, and restores it on
    unload
    -also it is blocking out the tooltip
    -UNTESTED

--- bootstrap.js
+++ bootstrap.js
@@ -15,6 +15,80 @@ Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 XPCOMUtils.defineLazyGetter(myServices, 'as', function(){ return Cc['@mozilla.org/alerts-service;1'].getService(Ci.nsIAlertsService) });
 XPCOMUtils.defineLazyGetter(myServices, 'sss', function(){ return Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService) });
 
+/*start - windowlistener*/
+var windowListener = {
+	//DO NOT EDIT HERE
+	onOpenWindow: function (aXULWindow) {
+		// Wait for the window to finish loading
+		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		aDOMWindow.addEventListener("load", function () {
+			aDOMWindow.removeEventListener("load", arguments.callee, false);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}, false);
+	},
+	onCloseWindow: function (aXULWindow) {},
+	onWindowTitleChange: function (aXULWindow, aNewTitle) {},
+	register: function () {
+		// Load into any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}
+		// Listen to new windows
+		Services.wm.addListener(windowListener);
+	},
+	unregister: function () {
+		// Unload from any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.unloadFromWindow(aDOMWindow, aXULWindow);
+		}
+		//Stop listening so future added windows dont get this attached
+		Services.wm.removeListener(windowListener);
+	},
+	//END - DO NOT EDIT HERE
+	loadIntoWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		var PlacesToolbar = aDOMWndow.document.querySelector('#PlacesToolbar');
+		if (PlacesToolbar && aDOMWndow.PlacesToolbar) {
+			PlacesToolbar.removeAttribute('tooltip');
+			PlacesToolbar.removeAttribute('popupsinherittooltip');
+			aDOMWindow.PlacesToolbar.prototype._onMouseOver = function PT__onMouseOver(aEvent) {
+			    let button = aEvent.target;
+			    if (button.parentNode == this._rootElt && button._placesNode &&
+			        PlacesUtils.nodeIsURI(button._placesNode))
+			      aDOMWindow.XULBrowserWindow.setOverLink(aEvent.target._placesNode.title + ' (' + aEvent.target._placesNode.uri + ')', null);
+			}
+        		aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils).loadSheet(cssUri, 1);
+		}
+		
+	},
+	unloadFromWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		var PlacesToolbar = aDOMWndow.document.querySelector('#PlacesToolbar');
+		if (PlacesToolbar && aDOMWndow.PlacesToolbar) {
+			PlacesToolbar.setAttribute('tooltip', 'bhTooltip');
+			PlacesToolbar.setAttribute('popupsinherittooltip', 'true');
+			aDOMWindow.PlacesToolbar.prototype._onMouseOver = function PT__onMouseOver(aEvent) {
+			    let button = aEvent.target;
+			    if (button.parentNode == this._rootElt && button._placesNode &&
+			        PlacesUtils.nodeIsURI(button._placesNode))
+			      aDOMWindow.XULBrowserWindow.seOverLink(aEvent.target._placesNode.title + ' (' + aEvent.target._placesNode.uri + ')', null);
+			}
+			aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils).removeSheet(cssUri, 1);
+		}
+	}
+};
+/*end - windowlistener*/
+
 function startup(aData, aReason) {
 	self.aData = aData;
 	//var css = '';
@@ -28,12 +102,15 @@ function startup(aData, aReason) {
 		aBaseURI: null
 	}
 	cssUri = Services.io.newURI(newURIParam.aURL, newURIParam.aOriginCharset, newURIParam.aBaseURI);
-	myServices.sss.loadAndRegisterSheet(cssUri, myServices.sss.USER_SHEET);
+	//myServices.sss.loadAndRegisterSheet(cssUri, myServices.sss.USER_SHEET);
+	
+	windowListener.register();
 }
 
 function shutdown(aData, aReason) {
 	if (aReason == APP_SHUTDOWN) return;
 	
+	windowListener.unregister();
 	myServices.sss.unregisterSheet(cssUri, myServices.sss.USER_SHEET);
 }
 
